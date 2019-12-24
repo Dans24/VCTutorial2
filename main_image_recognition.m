b736e33dde2a75afa7d973e8a3e38b0cff78446a
Daniel Costa <daniel.costa1998@gmail.com>
2019-12-24 17:34:24 +0000

    Initial commit

---
+++ main_image_recognition.m
@@ -0,0 +1,22 @@
+function [segmented_image, segmented_image_noise, SNR, original_image, histogram, number] = main_image_recognition(gray_img_filename, noise_type, noise_params)
+    % Load Image
+    
+    % Introduce Noise
+    
+    SNR = [];
+    % Process Image
+    % filtering methods, contrast equalization, normalization, among other techniques
+    
+    % segmenting all of the coins in the image
+    
+    segmented_image = [];
+    segmented_image_noise = [];
+    % final segmentation result
+    
+    % count number of coins
+    
+    number = 0;
+    % histogram showing the distribution of object sizes
+    histogram = [];
+end
+
